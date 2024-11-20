8205efa4558275c5785c04d907809ce34417aa08
sierraxd <f3arblast@icloud.com>
2024-11-20 13:42:07 +0200

    [blackout-sched.sh] added error notify cause mfs forbid VPNs

--- .opt/bin/blackout-sched.sh
+++ .opt/bin/blackout-sched.sh
@@ -3,3 +3,4 @@
 date '+https://energy.volyn.ua/spozhyvacham/perervy-u-elektropostachanni/hrafik-vidkliuchen/!img/%d-%m-%y.jpg' \
 	| xargs curl -sL \
 	| swappy -f - \
+	|| notify-send -i error 'Failed to get blackout schedule'
